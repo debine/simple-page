1e252c4a9d0530f1d3d698661b6c048dbb65cfb4
aban <356585995@qq.com>
2026-05-09 15:51:00 +0800

    feat: add card

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">Card</div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+    name: 'Card'
+});
+</script>
+
+<style scoped lang="css">
+.card {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 16px;
+    margin: 16px;
+}
+</style>
