87fefca1b58cdde99c459aba35cb6f1173614c40
aban <356585995@qq.com>
2026-05-09 13:53:06 +0800

    feat: add buttom

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="button">按钮</button>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+    name: 'Button'
+});
+</script>
+
+<style lang="css" scoped>
+.button {
+    background-color: #007bff;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 4px;
+    cursor: pointer;
+}
+</style>
